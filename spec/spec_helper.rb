9e77b59d6289ced1d4d4e5cd8ad60719c7b99f0e
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-11-29 21:57:49 +0100

    Rspec / Guard: Init.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+$:.push File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'siriproxy'
+
+RSpec.configure do |config|
+  def config.escaped_path(*parts)
+    Regexp.compile(parts.join('[\\\/]'))
+  end unless config.respond_to? :escaped_path
+
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  # config.mock_with :rspec
+end
+
+# Include support files.
+Dir["#{VIMIUS_SPEC_PATH}/support/**/*.rb"].each { |f| require f }
